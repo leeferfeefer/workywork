f53467fc91beb299b4387872149a7faa1f0ebd94
leeferfeefer <danbfjr@gmail.com>
2021-06-07 14:11:22 -0400

    Added messaging from Firebase

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
-  View
+  View,
+  Alert
 } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+
+const START_BREAK = 'START_BREAK';
+const START_WORK = 'START_WORK';
 
 const App: () => Node = () => {
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      if (remoteMessage?.notification?.title === START_BREAK 
+        || remoteMessage?.notification?.title === START_WORK) {
+        startMusic();
+        showAlert(remoteMessage);
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  const startMusic = () => {
+    
+  }
+
+  const showAlert = (remoteMessage) => {
+    Alert.alert(
+      remoteMessage?.notification?.title, 
+      remoteMessage?.notification?.body,
+      [
+        {
+          text: "Got it",
+          onPress: killAlarm
+        }    
+      ]
+    );
+  };
+
+  // Stop music
+  const killAlarm = () => {
+    console.log("Alarm killed");
+  }
   
   return (
     <SafeAreaView>
