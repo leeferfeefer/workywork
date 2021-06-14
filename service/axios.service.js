41fb407882add8b04ea60126e4d7d2a4a0ceb161
leeferfeefer <danbfjr@gmail.com>
2021-06-14 16:21:23 -0400

    testing for production

--- service/axios.service.js
+++ service/axios.service.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { IP } from '@env';
 
 const port = 8080;
+const baseURL = `http://${IP}:${port}`;
 
 const instance = axios.create({
-    baseURL: `http://localhost:${port}`,
+    baseURL,
     timeout: 5000,
 });
 
@@ -19,8 +20,7 @@ const saveToken = async (token, uuid) => {
 
 const startTimer = async () => {
     try {
-        console.log(IP)
-        await instance.post('/init/test');
+        await instance.post('/init');
     } catch (error) {
         console.log("Could not send message to server: ", JSON.stringify(error));
         throw error;
